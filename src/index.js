09ff5c994156440d7da401c97a611c3415db30eb
Jim Ntare <jntare@Jims-BK-MacBook-Pro.local>
2021-10-26 23:44:52 +0200

    implement todo list app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,64 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const toDoList = [
+  {
+    index: 1,
+    description: 'wash the car',
+    completed: true
+  },
+  { 
+    index: 2,
+    description: 'go out to eat',
+    completed: false
+  },
+  { 
+    index: 3,
+    description: 'complete feature',
+    completed: false 
+  },
+  { 
+    index: 3,
+    description: 'complete feature',
+    completed: false 
+  }
+]
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+function WebPage(list) {
+  const container = document.createElement('div');
+  container.classList = 'todo-container';
 
-  return element;
+  const heading = document.createElement('h3');
+  heading.innerHTML = 'Today\'s To Do';
+
+  const todoInput = document.createElement('input');
+  todoInput.placeholder = 'Add to your list...';
+  todoInput.classList = 'input-todo'
+
+  const listConainer = document.createElement('ul');
+  listConainer.classList = 'list-container';
+
+  const clearBtn = document.createElement('button');
+  clearBtn.classList = 'clear-btn';
+  clearBtn.innerHTML = 'Clear all completed'
+
+  list.forEach(item => {
+    let listItem = document.createElement('li');
+    let checkBox = document.createElement('input');
+    checkBox.type = 'checkbox';
+    checkBox.classList = 'check-box';
+    listItem.innerHTML = item.description;
+
+    listItem.appendChild(checkBox)
+    listConainer.appendChild(listItem)
+  });
+
+  container.appendChild(heading)
+  container.appendChild(todoInput)
+  container.appendChild(listConainer)
+  container.appendChild(clearBtn)
+
+  return container;
 }
 
-document.body.appendChild(component());
+document.body.appendChild(WebPage(toDoList));
